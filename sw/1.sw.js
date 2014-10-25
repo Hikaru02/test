21cf4e9933d72821cbcd9c4ee7664a794b02ec50
Hikaru02 <oao.hikaru.oao@gmail.com>
2014-10-25 16:49:37 +0900

    update

--- sw/1.sw.js
+++ sw/1.sw.js
@@ -6,7 +6,6 @@ log('hellow!')
 
 self.oninstall       = e => { log(e) }
 self.onactivate      = e => { log(e) }
-self.onfetch         = e => { log(e) }
 self.onbeforeevicted = e => { log(e) }
 self.onevicted       = e => { log(e) }
 self.ongeofenceenter = e => { log(e) }
@@ -14,7 +13,11 @@ self.ongeofenceleave = e => { log(e) }
 self.onmessage       = e => { log(e) }
 self.onpush          = e => { log(e) }
 self.onsync          = e => { log(e) }
-
+self.onfetch         = e => { 
+	log(e)
+	let url = e.request.url
+	e.respondWith(new Response('you try accsess to ' + url ))
+}
 
 
 
